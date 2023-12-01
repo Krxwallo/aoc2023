97b9c3a81342acf0529adabf99b10e8d85d49209
Krxwallo <59575572+krxwallo@users.noreply.github.com>
2023-12-01 20:21:08 +0100

    add ugly solution for day 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,16 +1,48 @@
+val digitStrings = listOf(
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+)
+
+val reversedDigitStrings = buildList {
+    digitStrings.forEach {
+        add(it.reversed())
+    }
+}
+
 fun main() = day(1) {
 
-    val input = inputString.split("\n\n").asSequence()
-        .map { g -> g.lines().sumOf { it.toInt() } }
+    val input = inputString.split("\n")
 
     part1 {
-        input.max()
+        input.sumOf {
+            (it.first { f -> f.isDigit() }.toString() + it.last { f -> f.isDigit() }).toInt()
+        }
     }
 
     part2 {
-        input.sortedDescending().take(3).sum()
+        input.sumOf {
+            fun compute(input: String, reversed: Boolean): Int {
+                val myDigitStrings = if (reversed) reversedDigitStrings else digitStrings
+
+                val firstStringDigitPair = input.findAnyOf(myDigitStrings)
+                val firstStringDigitIndex = firstStringDigitPair?.first
+                val firstStringDigitString = firstStringDigitPair?.second
+                val firstStringDigit = myDigitStrings.indexOf(firstStringDigitString).takeIf { it != -1 }?.plus(1)
+
+                val firstRealDigit = input.firstOrNull { f -> f.isDigit() }
+                val firstRealDigitIndex = input.indexOf(firstRealDigit ?: '_').takeIf { i -> i != -1 }
+                return if (firstStringDigit == null) firstRealDigit!!.digitToInt()
+                    else if (firstRealDigit == null) firstStringDigit
+                    else if (firstStringDigitIndex!! < firstRealDigitIndex!!) firstStringDigit
+                    else firstRealDigit.digitToInt()
+            }
+
+            val firstDigit = compute(it, false)
+            val lastDigit = compute(it.reversed(), true)
+
+            (firstDigit.toString() + lastDigit).toInt()
+        }
     }
 
-    expectPart1 = 24000
-    expectPart2 = 45000
+    expectPart1 = 142
+    expectPart2 = 281
 }
