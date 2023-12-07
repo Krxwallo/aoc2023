39522efec742c2aab97a3869a7054ad82184af5c
Krxwallo <59575572+krxwallo@users.noreply.github.com>
2023-12-07 08:05:50 +0100

    improve sorting (day 7)

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -2,15 +2,12 @@ import kotlin.math.pow
 
 val order = listOf("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A")
 
-// get a big number
-val p = (order.size + 1.0).pow(6)
-
 data class Hand(val data: List<Int>, val bid: Int) {
     val cardValue = data.reversed().withIndex().sumOf {
         it.value * (order.size + 1.0).pow(it.index.toDouble())
     }
 
-    fun typeValue(allowJoker: Boolean): Double {
+    fun typeValue(allowJoker: Boolean): Int {
         var sortedData = data.sorted()
 
         var countsList = buildList {
@@ -36,15 +33,13 @@ data class Hand(val data: List<Int>, val bid: Int) {
 
         val counts = countsList.map { it.size }
 
-        fun p(i: Int) = p * i
-
-        return if (counts.first() == 5) p(7)
-        else if (counts.first() == 4) p(6)
-        else if (counts.first() == 3 && counts[1] == 2) p(5)
-        else if (counts.first() == 3) p(4)
-        else if (counts.first() == 2 && counts[1] == 2) p(3)
-        else if (counts.first() == 2) p(2)
-        else if (counts.size == 5) p(1) else 0.0
+        return if (counts.first() == 5) 7
+        else if (counts.first() == 4) 6
+        else if (counts.first() == 3 && counts[1] == 2) 5
+        else if (counts.first() == 3) 4
+        else if (counts.first() == 2 && counts[1] == 2) 3
+        else if (counts.first() == 2) 2
+        else if (counts.size == 5) 1 else throw RuntimeException()
     }
 }
 
@@ -61,9 +56,10 @@ fun main() = day(7) {
 
     part1 {
         // Sort hands
-        val rankedHands = getHands(false).sortedBy {
-            it.typeValue(false) + it.cardValue
-        }
+        val rankedHands = getHands(false).sortedWith(compareBy(
+            { it.typeValue(false) },
+            { it.cardValue }
+        ))
 
         // Sum up
         rankedHands.withIndex().sumOf {
@@ -73,9 +69,10 @@ fun main() = day(7) {
 
     part2 {
         // Sort hands
-        val rankedHands = getHands(true).sortedBy {
-            it.typeValue(true) + it.cardValue
-        }
+        val rankedHands = getHands(true).sortedWith(compareBy(
+            { it.typeValue(true) },
+            { it.cardValue }
+        ))
 
         // Sum up
         rankedHands.withIndex().sumOf {
