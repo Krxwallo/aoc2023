767c2fa827bf5bb04a439ad4111993a4eb16d14c
Krxwallo <59575572+krxwallo@users.noreply.github.com>
2023-12-10 14:29:26 +0100

    add solution for 10/1

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,13 +1,10 @@
 
-
-
 fun main() = day(10) {
     val pipes = inputLines.readGrid()
-
     val start = pipes.firstNotNullOf { if (it.value == 'S') it.key else null }
 
-    fun connectedTo(pipe: Pos): List<Pos> {
-        val c = pipes[pipe] ?: return emptyList()
+    fun connectionsFor(pipe: Pos): List<Pos> {
+        pipes[pipe] ?: return emptyList()
         val x = pipe.x
         val y = pipe.y
         return when (pipes[pipe]) {
@@ -22,73 +19,25 @@ fun main() = day(10) {
         }
     }
 
-    fun connected(pos1: Pos, pos2: Pos): Boolean {
-        val connected1 = connectedTo(pos1)
-        val connected2 = connectedTo(pos2)
-        val connected = connected1.any { it == pos2 } && connected2.any { it == pos1 }
-        return connected
-    }
-
-    var tryedPositions = sequenceOf<Pos>()
 
     part1 {
-        /*fun tryPos(pos: Pos): Int {
-            tryedPositions += pos
-            return pos.neighbours.maxOf { n ->
-                val pipe = charGrid[n] ?: return@maxOf 0
-                val connected = connected(n, pos)
-                if (!connected) return@maxOf 0
-                if (n in tryedPositions) {
-                    return@maxOf 0
-                }
-                return@maxOf tryPos(n) + 1
-            }
+        var lastPipe: Pos? = null
+        var pipe: Pos = start
+        repeat(Int.MAX_VALUE) { counter ->
+            val next = connectionsFor(pipe).firstOrNull {
+                pipes[it] != null
+                        && it != lastPipe
+                        && it != start
+                        && pipe in connectionsFor(it)
+            } ?: return@part1 (counter + 1) / 2
+            lastPipe = pipe
+            pipe = next
         }
-        (tryPos(start) + 1) / 2*/
-        var nextPositions = connectedTo(start)
-        var previousPositions = emptyList<Pos>()
-        var previousPositions1 = emptyList<Pos>()
-        var steps = 0
-
-        while (nextPositions.any { it !in previousPositions }) {
-            println("Trying for $nextPositions | $previousPositions")
-            previousPositions = previousPositions1.toList()
-            previousPositions1 = nextPositions.toList()
-            nextPositions = buildList {
-                for (n in nextPositions.toList()) {
-                    val successors = connectedTo(n)
-                    for (s in successors) {
-                        pipes[s] ?: continue
-                        if (s in previousPositions) continue
-
-                        val c = connected(n, s)
-                        if (c) add(s)
-                    }
-                }
-            }
-            steps++
-        }
-
-        fun tryPos(pos: Pos): Int {
-            tryedPositions += pos
-            return pos.neighbours.maxOf { n ->
-                val pipe = pipes[n] ?: return@maxOf 0
-                val connected = connected(n, pos)
-                if (!connected) return@maxOf 0
-                if (n in tryedPositions) {
-                    return@maxOf 0
-                }
-                return@maxOf tryPos(n) + 1
-            }
-        }
-        steps
     }
 
     part2 {
 
     }
 
-
-    expectPart1 = null
     expectPart2 = null
 }
